e85d070d1821a265a587c0051af74020d18f20d5
PC Artem <stepin_evgen@mail.ru>
2023-05-01 17:48:06 +0300

    c# programm

--- ControlTask001/Program.cs
+++ ControlTask001/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayA =  {"techno", "by", "programma", "123hello", "Yes"};
+int countA = CountStringLessThreeChar(arrayA);
+string[] resultA =  GetResultArray(arrayA,countA);
+
+
+string[] arrayB = {"23", "Hi", "-4", "sun", "tom"};
+int countB  = CountStringLessThreeChar(arrayB);
+string[] resultB = GetResultArray(arrayB,countB);
+
+
+string[] arrayC = {"number", "lux", "chek", "Ru"};
+int countC = CountStringLessThreeChar(arrayC);
+string[] resultC = GetResultArray(arrayC, countC);
+
+void PrintArray(string[] array) 
+    {
+
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+                else Console.Write($"{array[i]}.");
+            }
+        Console.WriteLine();
+    }
+
+int CountStringLessThreeChar(string[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3) count++;
+            }
+        return count;
+    }
+
+string[] GetResultArray(string[] array, int count)
+    {
+        string[] result = new string[count];
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                    {
+                        result[result.Length - count] = array[i];
+                        count--;
+                    }
+            }
+        return result;
+    }
+
+PrintArray(resultA);
+PrintArray(resultB);
+PrintArray(resultC);
